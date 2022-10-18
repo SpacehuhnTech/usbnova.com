dcfb094bb213313f83f8c66745d548cccc1d22e8
Spacehuhn <github@spacehuhn.de>
2022-10-18 12:18:04 +0200

    Improved images

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         {/*<h1 className="hero__title">{siteConfig.title}</h1>*/}
-        <img src='/img/feature_logo.svg' alt='USBNova Logo' height='80px' />
+        <img src='/img/feature_logo.png' alt='USBNova Logo' height='80px' />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
