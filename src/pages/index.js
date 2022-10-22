fb8892dab20c124778a302ddeb048e64d42f4307
Spacehuhn <github@spacehuhn.de>
2022-10-22 22:18:50 +0200

    Added description

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="An easy-to-use BadUSB for learning and testing.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
